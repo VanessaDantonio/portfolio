8c39b09dc66790337549d6e846b07c6959d4e7ce
[Vanessa D'Antonio] <[yumin.dantonio@gmail.com]>
2023-04-09 18:47:17 +0100

    navbar useState is working now

--- src/components/Navbar.js
+++ src/components/Navbar.js
@@ -1,36 +1,37 @@
-import React, { useState } from "react";
+import React, { Component } from "react";
 import { Link, NavLink } from "react-router-dom";
 import { FontAwesomeIcon } from "@fortawesome/react-fontawesome";
 import { faBars, faClose } from "@fortawesome/free-solid-svg-icons";
 import { faLinkedin, faGithub, faInstagram, faTwitter } from "@fortawesome/free-brands-svg-icons";
 
-export const Navbar = () => {
-const { nav, setNav } = useState(false);
-const handleClick = () => setNav(!nav);
+export class Navbar extends Component {
+  state = { clicked: false };
+  handleClick = () => {this.setState({clicked: !this.state.clicked})};
 
+  render() {
   return (
     <div className='fixed w-full h-[70px] flex justify-between items-center px-4 bg-[#0a192f] text-gray-300'>
-      <Link className="logo text-3xl" to="/" style={{textShadow: "4px 4px 4px #aaa"}} onClick={() => setNav(false)}>Vanessa D'Antonio</Link>
+      <Link className="logo text-3xl" to="/" style={{textShadow: "4px 4px 4px #aaa"}}>Vanessa D'Antonio</Link>
       
       {/* Menu */}
-      <ul className="hidden md:flex">
-        <NavLink exact="true" activeclassname="active" to="/" onClick={() => setNav(false)} className="navLink">Home</NavLink>
-        <NavLink exact="true" activeclassname="active" to="/about" onClick={() => setNav(false)} className="navLink">About</NavLink>
-        <NavLink exact="true" activeclassname="active" to="/work" onClick={() => setNav(false)} className="navLink">Work</NavLink>
-        <NavLink exact="true" activeclassname="active" to="/contact" onClick={() => setNav(false)} className="navLink">Contact</NavLink>
+      <ul id='navbar' className="hidden md:flex">
+        <NavLink exact="true" activeclassname="active" to="/" className="navLink">Home</NavLink>
+        <NavLink exact="true" activeclassname="active" to="/about" className="navLink">About</NavLink>
+        <NavLink exact="true" activeclassname="active" to="/work" className="navLink">Work</NavLink>
+        <NavLink exact="true" activeclassname="active" to="/contact" className="navLink">Contact</NavLink>
       </ul>
       
       {/* Hamburger */}
-      <div onClick = { handleClick } className="hamburger-icon md:hidden z-10">
-        {!nav ? <FontAwesomeIcon icon = { faBars }/> : <FontAwesomeIcon icon = { faClose }/>}
+      <div onClick = { this.handleClick } className="hamburger-icon md:hidden z-10">
+        {this.state.clicked ? <FontAwesomeIcon icon = { faClose }/> : <FontAwesomeIcon icon = { faBars }/>}
       </div>
       
       {/* Mobile menu */}
-      <ul className={!nav ? "hidden" : "absolute top-0 left-0 w-full h-screen bg-[#0a192f] flex flex-col justify-center items-center"}>
-        <NavLink exact="true" activeclassname="active" to="/" onClick={() => setNav(false)} className="navLink py-4 text-2xl">Home</NavLink>
-        <NavLink exact="true" activeclassname="active" to="/about" onClick={() => setNav(false)} className="navLink py-4 text-2xl">About</NavLink>
-        <NavLink exact="true" activeclassname="active" to="/work" onClick={() => setNav(false)} className="navLink py-4 text-2xl">Work</NavLink>
-        <NavLink exact="true" activeclassname="active" to="/contact" onClick={() => setNav(false)} className="navLink py-4 text-2xl">Contact</NavLink>
+      <ul className={this.state.clicked ? "absolute top-0 left-0 w-full h-screen bg-[#0a192f] flex flex-col justify-center items-center" : "hidden"}>
+        <NavLink onClick={() => this.setState({clicked: false})} exact="true" activeclassname="active" to="/" className="navLink py-4 text-2xl">Home</NavLink>
+        <NavLink onClick={() => this.setState({clicked: false})} exact="true" activeclassname="active" to="/about" className="navLink py-4 text-2xl">About</NavLink>
+        <NavLink onClick={() => this.setState({clicked: false})} exact="true" activeclassname="active" to="/work" className="navLink py-4 text-2xl">Work</NavLink>
+        <NavLink onClick={() => this.setState({clicked: false})} exact="true" activeclassname="active" to="/contact" className="navLink py-4 text-2xl">Contact</NavLink>
       </ul>
 
       {/* Menu */}  
@@ -51,4 +52,5 @@ const handleClick = () => setNav(!nav);
 
     </div>
   );
+}
 }
